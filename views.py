d60facd31b5be4408da9e5ed92cddd91b85fc876
enzomatu1 <3nzocraft@gmail.com>
2026-08-14 15:48:24 -0300

    funny ass lightmode button

--- views.py
+++ views.py
@@ -1,5 +1,8 @@
 from main import app
 from flask import render_template
+lightmode = False
+
 @app.route("/")
 def homepage():
-    return render_template("index.html")
+    text = "shit"
+    return render_template("index.html",text=text)
