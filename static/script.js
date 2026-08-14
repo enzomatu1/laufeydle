2a2471c07f6ed585644311b19bee30ffebfd5f7a
enzomatu1 <3nzocraft@gmail.com>
2026-08-14 15:57:39 -0300

    quick delete and change to button lol (still ugly)

--- static/script.js
+++ static/script.js
@@ -1,3 +1,9 @@
 function toggleTheme() {
     document.body.classList.toggle("lightmode");
+    let btn = document.getElementById("themeButton");
+    if (document.body.classList.contains("lightmode")) {
+        btn.textContent = "🌙";
+    } else {
+        btn.textContent = "☀️";
+    }
 }
