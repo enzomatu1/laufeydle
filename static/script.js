b92b431935965edc8ca72e1dd7305929ba0ce865
enzomatu1 <3nzocraft@gmail.com>
2026-09-14 10:10:07 -0300

    just a couple tests

--- static/script.js
+++ static/script.js
@@ -2,8 +2,8 @@ function toggleTheme() {
     document.body.classList.toggle("lightmode");
     let btn = document.getElementById("themeButton");
     if (document.body.classList.contains("lightmode")) {
-        btn.textContent = "🌙";
+        btn.innerHTML = '<img src="static/lua.png" alt="Dark mode" width="40" height="40">';
     } else {
-        btn.textContent = "☀️";
+        btn.innerHTML = '<img src="static/sun.png" alt="Dark mode" width="40" height="40">';
     }
 }
