9b289c5f91e7e3ac7398e3aadb84db32f8f4acbd
enzomatu1 <3nzocraft@gmail.com>
2026-09-17 15:54:36 -0300

    better linkage and model placement

--- static/script.js
+++ static/script.js
@@ -1,9 +1,3 @@
 function toggleTheme() {
     document.body.classList.toggle("lightmode");
-    let btn = document.getElementById("themeButton");
-    if (document.body.classList.contains("lightmode")) {
-        btn.innerHTML = '<img src="static/lua.png" alt="Dark mode" width="40" height="40">';
-    } else {
-        btn.innerHTML = '<img src="static/sun.png" alt="Dark mode" width="40" height="40">';
-    }
 }
